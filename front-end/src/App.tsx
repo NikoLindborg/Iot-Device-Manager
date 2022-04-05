1547d0a53547d7035532940a9f39680ea4f4407d
jaanikaukonen <kaukonen.jaani@gmail.com>
2022-04-05 17:31:48 +0300

    Added base view in app and components inside base view.

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,28 +1,11 @@
 import React from 'react'
 import './App.css'
-import StatusComponent from './components/StatusComponent/StatusComponent'
-import ChannelListComponent from './components/ChannelListComponent/ChannelListComponent'
-import DeviceListComponent from './components/DeviceListComponent/DeviceListComponent'
-import ChannelIcon from './assets/icons/channel_icon.svg'
-import StatusRedIcon from './assets/icons/status_red.svg'
+import BaseView from './views/base/BaseView'
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <StatusComponent></StatusComponent>
-      <ChannelListComponent
-        componentItems={{
-          icon: ChannelIcon,
-          label: 'Temperature',
-        }}
-      ></ChannelListComponent>
-      <DeviceListComponent
-        componentItems={{
-          icon: StatusRedIcon,
-          label: 'Raspberry Pi',
-          info: 'Device is offline',
-        }}
-      ></DeviceListComponent>
+      <BaseView />
     </div>
   )
 }
--- front-end/src/views/base/BaseView.tsx
+++ front-end/src/views/base/BaseView.tsx
@@ -1,8 +1,13 @@
 import React, {useEffect, useState} from 'react'
+import ChannelListComponent from '../../components/ChannelListComponent/ChannelListComponent'
+import DeviceListComponent from '../../components/DeviceListComponent/DeviceListComponent'
 import MenuComponent from '../../components/menu/MenuComponent'
 import MobileMenuComponent from '../../components/menu/mobile/MobileMenuComponent'
 import MobileNavigationComponent from '../../components/navigation/mobile/MobileNavigationComponent'
 import NavigationComponent from '../../components/navigation/NavigationComponent'
+import StatusComponent from '../../components/StatusComponent/StatusComponent'
+import ChannelIcon from '../../assets/icons/channel_icon.svg'
+import StatusRedIcon from '../../assets/icons/status_red.svg'
 import './BaseView.css'
 
 const BaseView: React.FC = () => {
@@ -41,12 +46,20 @@ const BaseView: React.FC = () => {
       </div>
       <div className="base-view-content-container">
         {/* Content comes here */}
-        <h1>Device</h1>
-        <h1>Device</h1>
-        <h1>Device</h1>
-        <h1>Device</h1>
-        <h1>Device</h1>
-        <h1>Device</h1>
+        <StatusComponent></StatusComponent>
+        <ChannelListComponent
+          componentItems={{
+            icon: ChannelIcon,
+            label: 'Temperature',
+          }}
+        ></ChannelListComponent>
+        <DeviceListComponent
+          componentItems={{
+            icon: StatusRedIcon,
+            label: 'Raspberry Pi',
+            info: 'Device is offline',
+          }}
+        ></DeviceListComponent>
       </div>
     </div>
   )

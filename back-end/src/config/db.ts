732e6e4faad8467384152d4ead35e3d2587a3124
NikoLindborg <nlindborg@saunassa.com>
2022-04-24 12:43:15 +0300

    adding console log to pipelines for debugging

--- back-end/src/config/db.ts
+++ back-end/src/config/db.ts
@@ -1,17 +1,17 @@
-import mongoose from "mongoose"
+import mongoose from 'mongoose'
 
 const url = process.env.MONGODB_URL
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(url)
-        console.log('url', url)
-        console.log(`MongoDB connected: ${conn.connection.host}`)
-    } catch (error) {
-        console.log(error)
-        process.exit(1)
-    }
+  try {
+    const conn = await mongoose.connect(url)
+    console.log('url', url)
+    console.log(`MongoDB connected: ${conn.connection.host}`)
+  } catch (error) {
+    console.log('url', url)
+    console.log(error)
+    process.exit(1)
+  }
 }
 
 export default connectDB
-
